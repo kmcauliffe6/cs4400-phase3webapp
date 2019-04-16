e0a3cf18a60f7ea9196fd92ecea0743e88b9f9dd
kmcauliffe6 <mcauliffek@gatech.edu>
2019-04-16 16:21:26 -0400

    added registration navigation page

--- phase3.py
+++ phase3.py
@@ -11,10 +11,44 @@ connection = pymysql.connect(host='localhost',
 cursor = connection.cursor()
 
 @app.route("/")
-def hello():
+def setup():
+    return render_template('home.html')
+
+#registration methods
+@app.route('/registernavigation')
+def registernavigation():
+    return render_template('register_navigation.html')
+
+@app.route('/back')
+def back_to_home():
+    return render_template('home.html')
+
+@app.route('/register_user')
+def register_user():
+    return render_template('home.html')
+
+@app.route('/register_visitor')
+def register_visitor():
+    return render_template('home.html')
+
+@app.route('/register_employee')
+def register_employee():
+    return render_template('home.html')
+
+@app.route('/register_employee_visitor')
+def register_employee_visitor():
     return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+""""sql = "SELECT username FROM User WHERE username = 'lara'"
+    result = cursor.execute(sql)
+    answer = ""
+    for row in cursor:
+        answer += row['username']
+"""
+
 
 
