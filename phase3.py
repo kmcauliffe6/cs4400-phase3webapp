18a09bc23aa71e94af75f07cc68df671090f0e71
kmcauliffe6 <mcauliffek@gatech.edu>
2019-04-19 16:07:32 -0400

    started getting table values for view transit history

--- phase3.py
+++ phase3.py
@@ -237,7 +237,7 @@ def take_transit():
     return render_template('user_take_transit.html', sites = sites)
 @app.route('/view_transit_history')
 def view_transit_history():
-    sql = "SELECT Sname FROM Site"
+    sql = "SELECT SiteName FROM Site"
     cursor.execute(sql)
     sites = cursor.fetchall()
     return render_template('user_view_transit_history.html', sites = sites)
@@ -254,10 +254,29 @@ def go_to_user_type_functionality():
 #transit methods
 @app.route('/filter_transit_buttonClick',methods=['GET','POST'])
 def filter_transit_buttonClick():
-    sql = "SELECT * FROM User"
+    siteFilter = request.form['contain_site']
+    transportTypeFilter = request.form['transport_type']
+    lowerPriceFilter = request.form['lower']
+    upperPriceFilter = request.form['upper']
+
+    #sql = "SELECT TransitRoute AS Route, TransitType AS Transport Type, Transit Price AS Price, COUNT(SiteName)) FROM Transit Join Connect ON Transit.TransitType = Connect.TransitType AND Transit.TransitRoute = Connect.TransitRoute WHERE ((TransitPrice BETWEEN '{lower}' AND '{upper}') AND (SiteName = '{sname}' AND TransitType = '{ttype}'));".format(lower = lowerPriceFilter, upper = upperPriceFilter, sname = siteFilter, ttype = transportTypeFilter)
+    #not working rn, come back
+    sql = "SELECT * FROM Transit"
     cursor.execute(sql)
     data = cursor.fetchall()
     return render_template('user_take_transit.html', data=data)
+
+@app.route('/filter_transit_history_buttonClick',methods=['GET','POST'])
+def filter_transit__history_buttonClick():
+    transportTypeFilter = request.form['transport_type']
+    siteFilter = request.form['contain_site']
+    routeFilter = request.form['route']
+    startDateFilter = request.form['start_date']
+    endDateFilter = request.form['end_date']
+    sql = "SELECT * FROM Transit"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    return render_template('user_view_transit_history.html', data=data)
 
 #helper methods
 def getUserType(username):
